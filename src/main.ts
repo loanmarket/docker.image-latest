daee3289725271e87354b6c208f768126b374ba9
Joe Patterson <jrwpatterson@gmail.com>
2020-08-12 21:22:08 +1000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,57 @@
 import * as core from '@actions/core'
-import axios from 'axios'
-import * as _ from 'lodash'
-import * as semver from 'semver'
+import {graphql} from '@octokit/graphql'
+// import * as _ from 'lodash'
+// import * as semver from 'semver'
 
 async function run(): Promise<void> {
   try {
-    const imageName: string = core.getInput('image_name', {required: true})
+    const owner: string = core.getInput('github_owner', {required: true})
+    const repoName: string = core.getInput('image_name', {required: true})
+    const token: string = core.getInput('github_token', {required: true})
+    const graphqlWithAuth = graphql.defaults({
+      headers: {
+        authorization: `token ${token}`
+      }
+    })
 
-    axios
-      .get(`https://hub.docker.com/v2/repositories/${imageName}/tags/`)
-      .then(response => {
-        const latest = _.last(
-          _.remove(_.sortBy(_.map(response.data.results, 'name')), function(e) {
-            return semver.validRange(e)
-          })
-        )
-        core.setOutput('latest', latest)
-        core.exportVariable('latest', latest)
-      })
+    const {getLatest} = await graphqlWithAuth(
+      `
+      query getLatest($organisation: String!, $repoName: String!) {
+        organization(login: $organisation) {
+          packages(first: 100, names: [$repoName]) {
+            nodes {
+              id
+              name
+              versions(first: 100) {
+                nodes {
+                  version
+                }
+              }
+              latestVersion {
+                version
+              }
+            }
+            totalCount
+          }
+        }
+      }`,
+      {
+        owner,
+        repoName
+      }
+    )
+    console.log(JSON.stringify(getLatest))
+    // axios
+    //   .get(`https://hub.docker.com/v2/repositories/${imageName}/tags/`)
+    //   .then(response => {
+    //     const latest = _.last(
+    //       _.remove(_.sortBy(_.map(response.data.results, 'name')), function(e) {
+    //         return semver.validRange(e)
+    //       })
+    //     )
+    //     core.setOutput('latest', latest)
+    //     core.exportVariable('latest', latest)
+    //   })
   } catch (error) {
     core.setFailed(error.message)
   }
